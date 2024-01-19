f552521a5694d023e1b90d661e1dbe19ffdb52bc
Maxruizdz <maxruizdz@hotmail.com>
2024-01-19 09:45:08 -0300

    Programando Listas Enlazadas

---
+++ ConsoleApp1/ListaEnlazada.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    public class ListaEnlazada
+    {
+        protected Nodo Cabeza { get; set; }
+        protected Nodo cola  { get; set; }
+
+
+        public ListaEnlazada(Nodo primerNodo) {
+
+            Cabeza = primerNodo;
+            cola = primerNodo;
+        }
+
+        public void InsertarNodoAlfinal(Nodo nuevo_nodo) {
+            cola.siguiente = nuevo_nodo;
+            cola = cola.siguiente;
+        }
+
+        public void borrarNodo(Nodo nodo) {
+
+            nodo.siguiente = null;
+        
+        }
+
+        public Nodo Buscar_nodo(int valor_A_buscar) {
+            Nodo Nodo_Actual = Cabeza;
+        while (Nodo_Actual != null) {
+                if (Nodo_Actual.valor == valor_A_buscar)
+                {
+
+
+                    return Nodo_Actual;
+
+                }
+                else {
+                    Nodo_Actual = Nodo_Actual.siguiente;
+                }
+            
+            
+            
+            
+            }
+
+            return null;
+        }
+
+
+        public void BorrarNodoPorValor(int Valor_A_Borra) {
+
+            Nodo NodoABorrar = Buscar_nodo(Valor_A_Borra);
+            if (NodoABorrar != null)
+            {
+                borrarNodo(NodoABorrar);
+            }
+
+           
+        
+        
+        }
+    }
+}
---
+++ ConsoleApp1/Nodo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Nodo
+    {
+        public int  valor { get; set; }
+       public Nodo siguiente { get; set; }
+
+        public Nodo anterior { get; set; }
+
+        public Nodo(int x) {
+        
+        valor = x;
+        siguiente = null;
+        anterior = null;
+
+
+        }
+
+
+    public void AgregarNodo_Siguiente(Nodo Nodo_Siguiente) {
+        
+        
+       siguiente= Nodo_Siguiente;
+        
+        
+        
+        }
+
+        public void AgregarNodo_Anterior(Nodo Nodo_anterior) {
+        this.anterior = Nodo_anterior;
+        
+        }
+
+        public int ActualizarValor(int nuevoValor) {
+        this.valor = nuevoValor;
+        return this.valor;
+        
+        }
+
+    }
+}
