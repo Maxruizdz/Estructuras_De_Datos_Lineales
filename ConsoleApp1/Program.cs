d4ed8490a3b8f7d9e4f339b80a57026b30c60c29
Maxruizdz <maxruizdz@hotmail.com>
2024-01-22 09:42:05 -0300

    Resolviendo-Odd-Even-LinkedList

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1;
+
+ListaEnlazada listaEnlazada = new ListaEnlazada(new Nodo(1));
+
+listaEnlazada.InsertarNodoAlfinal(new Nodo(2));
+listaEnlazada.InsertarNodoAlfinal(new Nodo(3));
+listaEnlazada.InsertarNodoAlfinal(new Nodo(4));
+
+
+Console.WriteLine("Lista Original:");
+listaEnlazada.MostrarLista();
+
+listaEnlazada.OddEvent_LinkedList();
+
+Console.WriteLine("Lista Reorganizada:");
+listaEnlazada.MostrarLista();
